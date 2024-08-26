a88d1863a826059bf37b8174186c67a9b294fbfd
Sofia Navales <sofinavales@gmail.com>
2024-08-26 18:10:35 -0500

    Creación clase producto

---
+++ Producto.cs
@@ -0,0 +1,12 @@
+public class Producto
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public decimal Precio { get; set; }
+
+    public Producto(int id, string nombre, decimal precio)
+    {
+        Id = id;
+        Nombre = nombre;
+        Precio = precio;
+    }
