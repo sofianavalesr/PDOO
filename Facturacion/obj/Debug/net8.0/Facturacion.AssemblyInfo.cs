1b36da43b6ed49a66fa75ee25f12fd66bd62a72f
Sofia Navales <sofinavales@gmail.com>
2024-09-10 17:41:31 -0500

    Actualizacion asesoria

--- Facturacion/obj/Debug/net8.0/Facturacion.AssemblyInfo.cs
+++ Facturacion/obj/Debug/net8.0/Facturacion.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Facturacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c87701ba539e7c34376bc0d7ea649787e6373ff9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b69d0c7c32eeff458a34016dd3af6c4a7c565830")]
 [assembly: System.Reflection.AssemblyProductAttribute("Facturacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Facturacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
