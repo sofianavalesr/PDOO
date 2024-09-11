a5f89b74e8120ff5b1df5a9f1ec6ae959f1b8d7a
Paula Olier <paulaolier@gmail.com>
2024-09-10 20:22:06 -0500

    Actualizacion clase producto

--- Facturacion/Producto.cs
+++ Facturacion/Producto.cs
@@ -1,13 +1,12 @@
 namespace Facturacion;
 public class Producto
 {
-    private int Id { get; set; }
-    private string Nombre { get; set; }
-    private decimal Precio { get; set; }
 
-    public Producto(){
+    public int Id { get; set; }
+    public  string Nombre { get; set; }
+    public decimal Precio { get; set; }
 
-    }
+    
     public Producto(int id, string nombre, decimal precio)
     {
         Id = id;
